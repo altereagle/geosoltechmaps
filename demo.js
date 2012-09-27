45f2d0635279ee134fb0206918dc9d82fd262a0b
Kendell Joseph <kendelljoseph@gmail.com>
2012-09-27 12:59:21 -0400

    HTML, added live demo maps, JS, too many changes to list

--- demo.js
+++ demo.js
@@ -1,6 +1,33 @@
 maphard.get.googleTable({
     key: "0AkOIIIT7wBStdC13dEh2V0xMMUdVSnhrOWV0dy1QRnc",
     callback: function(data){
-        console.log(data);
+        //console.log(data);
     }
 });
+
+function initGMap() {
+    var mapId = "googleMap";
+    var mapOptions = {
+        center: new google.maps.LatLng(30.2669, -97.7428),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    
+    maphard.load.googleMap(mapId, mapOptions);
+}
+
+function initOMap(){
+    var mapId = "openMap";
+    var mapOptions = {
+        layer: "default"
+    };
+        
+    maphard.load.osMap(mapId,mapOptions)
+}
+
+function initAllMaps(){
+    initGMap();
+    initOMap();
+}
+
+$(document).ready(initAllMaps);
